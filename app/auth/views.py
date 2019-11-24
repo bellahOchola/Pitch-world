205372fc1a68e9933b07f50be3823b32dcd1ff06
bellahOchola <bellahkenya@gmail.com>
2019-11-24 13:33:29 +0300

    create a route for user login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,22 +1,22 @@
-# from flask import render_template,redirect, url_for, flash, request
-# from . import auth
-# from flask_login import login_required,login_user,logout_user
-# from .. import db
-# from .forms import SignUpForm,LoginForm
-# from ..models import User
+from flask import render_template,redirect, url_for, flash, request
+from . import auth
+from flask_login import login_required,login_user,logout_user
+from .. import db
+from .forms import SignUpForm,LoginForm
+from ..models import User
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+# @auth.route('/login', methods=['GET', 'POST'])
+# def login():
+#     login_form = LoginForm()
+#     if login_form.validate_on_submit():
+#         user = User.query.filter_by(email = login_form.email.data).first()
+#         if user is not None and user.verify_password(login_form.password.data):
+#             login_user(user,login_form.remember.data)
+#             return redirect(request.args.get('next') or url_for('main.index'))
 
-        flash('Invalid username or Password')
+#         flash('Invalid username or Password')
 
-    return render_template('auth/login.html',login_form = login_form)
+#     return render_template('auth/login.html',login_form = login_form)
     
 
 @auth.route('/signup',methods = ["GET","POST"])
