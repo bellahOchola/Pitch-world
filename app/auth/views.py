eb443f4a9cdd980ffd65d4c6ccd57d9a033c985c
bellahOchola <bellahkenya@gmail.com>
2019-11-24 13:33:57 +0300

    create a route for new users to sign up

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,29 +5,29 @@ from .. import db
 from .forms import SignUpForm,LoginForm
 from ..models import User
 
-# @auth.route('/login', methods=['GET', 'POST'])
-# def login():
-#     login_form = LoginForm()
-#     if login_form.validate_on_submit():
-#         user = User.query.filter_by(email = login_form.email.data).first()
-#         if user is not None and user.verify_password(login_form.password.data):
-#             login_user(user,login_form.remember.data)
-#             return redirect(request.args.get('next') or url_for('main.index'))
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-#         flash('Invalid username or Password')
+        flash('Invalid username or Password')
 
-#     return render_template('auth/login.html',login_form = login_form)
+    return render_template('auth/login.html',login_form = login_form)
     
 
-@auth.route('/signup',methods = ["GET","POST"])
-def signup():
-    form = SignUpForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-    return render_template('auth/signup.html', signup_form = form)
+# @auth.route('/signup',methods = ["GET","POST"])
+# def signup():
+#     form = SignUpForm()
+#     if form.validate_on_submit():
+#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         return redirect(url_for('auth.login'))
+#     return render_template('auth/signup.html', signup_form = form)
 
 
 @auth.route('/logout')
