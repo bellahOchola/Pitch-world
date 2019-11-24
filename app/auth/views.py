fcd6d108a8e3c8af6c4f45eab06bd92eb5c95258
bellahOchola <bellahkenya@gmail.com>
2019-11-24 13:34:36 +0300

    create a new folder auth in the templates folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,15 +19,15 @@ def login():
     return render_template('auth/login.html',login_form = login_form)
     
 
-# @auth.route('/signup',methods = ["GET","POST"])
-# def signup():
-#     form = SignUpForm()
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         return redirect(url_for('auth.login'))
-#     return render_template('auth/signup.html', signup_form = form)
+@auth.route('/signup',methods = ["GET","POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', signup_form = form)
 
 
 @auth.route('/logout')
