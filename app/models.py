2a753ab93747b4cb8fab6c9a182bf398628a3b6a
bellahOchola <bellahkenya@gmail.com>
2019-11-24 13:57:08 +0300

    add save and retrieve pitch functions in the pitches class

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(255), index = True)
     email = db.Column(db.String(255), unique = True, index= True)
     pass_secure= db.Column(db.String(255))
+    pitch = db.relationship('Pitches',backref = 'user',lazy = "dynamic")
     
 
     @property
@@ -35,3 +36,18 @@ class User(db.Model, UserMixin):
 class Pitches(db.Model):
     ___tablename__ = 'pitch'
 
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_category = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitch(cls,id):
+        pitch = Pitches.query.filter_by(pitch_id=id).all()
+        return pitch
