f5e3e3f03661bbb26c992fd6fca6d5fa1542cd8a
bellahOchola <bellahkenya@gmail.com>
2019-11-24 13:30:27 +0300

    import flaskform class in the auth/forms file

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField
-from wtforms.validators import Required,Email,EqualTo
-from ..models import User
-from wtforms import ValidationError
+# from flask_wtf import FlaskForm
+# from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField
+# from wtforms.validators import Required,Email,EqualTo
+# from ..models import User
+# from wtforms import ValidationError
 
 class SignUpForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
